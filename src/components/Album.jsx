a268f73184e49bdfffc71ba16eaa09dd14ac3425
ChiaraSoddu <chiara.soddu@hotmail.com>
2022-09-16 17:21:25 +0200

    react_15

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -18,38 +18,12 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 class Album extends React.Component {
-  /*  state = {
-    album: {},
-    songs: [],
-  }; */
+ 
 
   componentDidMount = async () => {
     let albumId = this.props.match.params.id;
     this.props.getAlbum(albumId);
-    /* let headers = new Headers({
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-      "X-RapidAPI-Key": "222902beabmshb95a65b737cead6p1f3ac9jsn23ced94c0d20",
-    });
-
-    try {
-      let response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/deezer/album/" + albumId,
-        {
-          method: "GET",
-          headers,
-        }
-      );
-
-      if (response.ok) {
-        let album = await response.json();
-        this.setState({
-          album,
-          songs: album.tracks.data,
-        });
-      }
-    } catch (exception) {
-      console.log(exception);
-    } */
+    
   };
 
   render() {
